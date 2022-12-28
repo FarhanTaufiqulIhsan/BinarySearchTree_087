29c5001ae3d9a2bda66e5f12db1ca55a01acb701
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2022-12-28 17:04:55 +0700

    Add project files.

---
+++ Activity8_BinarySearchTree/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity8_BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
