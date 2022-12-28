49b7de0b395dbfd55885af3eb9d5ce895660c89a
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2022-12-28 17:08:44 +0700

    Creating Node class

--- Activity8_BinarySearchTree/Program.cs
+++ Activity8_BinarySearchTree/Program.cs
@@ -8,6 +8,24 @@ namespace Activity8_BinarySearchTree
 {
     class Program
     {
+        class Node
+        {
+            public string info;
+            public Node leftchild;
+            public Node rightchild;
+
+            // COnstructor for the Node Class
+            public Node(string i, Node l, Node r)
+            {
+                info = i;
+                leftchild = l;
+                rightchild = r;
+            }
+
+        }
+
+        /* A node class consist of*/
+
         static void Main(string[] args)
         {
         }
