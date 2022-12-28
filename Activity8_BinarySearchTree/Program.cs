1d2085789fa79f66804edeffc9796f1f1e09ffaa
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2022-12-28 17:17:12 +0700

    Create program, search method

--- Activity8_BinarySearchTree/Program.cs
+++ Activity8_BinarySearchTree/Program.cs
@@ -21,9 +21,29 @@ namespace Activity8_BinarySearchTree
         }
 
     }
+    /* A node class consist of three things, the information, references to the right child, and references*/
+
     class Program
     {
-        /* A node class consist of three things, the information, references to the right child, and references*/
+        public Node ROOT;
+        public Program()
+        {
+            ROOT = null; /* Initializing ROOT to null */
+        }
+        public void search(string element, ref Node parent, ref Node currentNode)
+        {
+            /* This function serachs the currentNode of the specified Node as well as the currentNode of its parents */
+            currentNode = ROOT;
+            parent = null;
+            while ((currentNode != null) && (currentNode.info != element))
+            {
+                parent = currentNode;
+                if (string.Compare(element,currentNode.info) < 0)
+                    currentNode = currentNode.leftchild;
+                else
+                    currentNode = currentNode.rightchild;
+            }
+        }
 
         class BinaryTree
         {
